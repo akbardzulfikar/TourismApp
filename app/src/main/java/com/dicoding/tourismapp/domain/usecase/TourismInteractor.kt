6acad6c9334ff6c68686d2004be8df34c4eb8604
laksamana.akbar <[akbardzulfikar09@gmail.com]>
2020-11-09 08:36:07 +0700

    First commit

---
+++ app/src/main/java/com/dicoding/tourismapp/domain/usecase/TourismInteractor.kt
@@ -0,0 +1,13 @@
+package com.dicoding.tourismapp.domain.usecase
+
+import com.dicoding.tourismapp.domain.model.Tourism
+import com.dicoding.tourismapp.domain.repository.ITourismRepository
+
+class TourismInteractor(private val tourismRepository: ITourismRepository): TourismUseCase {
+
+    override fun getAllTourism() = tourismRepository.getAllTourism()
+
+    override fun getFavoriteTourism() = tourismRepository.getFavoriteTourism()
+
+    override fun setFavoriteTourism(tourism: Tourism, state: Boolean) = tourismRepository.setFavoriteTourism(tourism, state)
+}
