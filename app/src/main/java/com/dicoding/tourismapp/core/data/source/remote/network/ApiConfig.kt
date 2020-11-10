dd3dae0a1cb90ca7208a76e6ce34cb521d1c8c73
laksamana.akbar <[akbardzulfikar09@gmail.com]>
2020-11-10 13:26:25 +0700

    Third Commit,
    - Implement RxJava2 on retrofit
    - Implement Room + Rx
    - Convert Flowable become LiveData

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiConfig.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiConfig.kt
@@ -1,9 +1,9 @@
 package com.dicoding.tourismapp.core.data.source.remote.network
 
-import android.icu.util.TimeUnit
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
@@ -20,6 +20,7 @@ object ApiConfig {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://tourism-api.dicoding.dev/")
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(provideOkHttpClient())
             .build()
         return retrofit.create(ApiService::class.java)
